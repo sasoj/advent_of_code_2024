9c8c7a85a36f2b1b03e41ad68586724422735a91
Saso Jordanoski <saso.jordanoski@gmail.com>
2024-12-05 18:20:07 +0100

    day 5

---
+++ 5.py
@@ -0,0 +1,46 @@
+import numpy as np
+import lib
+# note first and second are combined
+def code():
+    precedences = np.zeros((100, 100), dtype='int')
+    print("First")
+    records = lib.records(5, '')
+    rule_set = set()
+    bad_updates = list()
+    for rule in records:
+        if rule.strip() == "":
+            break
+        before, after = [int(x) for x in rule.split('|')]
+        rule_set.add((after, before))
+        #print(before, after)
+        precedences[after, before] = 1
+    
+    result = 0
+    for update in records:
+        pages = np.fromstring(update, dtype='int', sep=',')
+        blocked = np.zeros(100, dtype='int')
+        for page in pages:
+            if blocked[page]:
+                bad_updates.append(pages)
+                break
+            blocked = blocked + precedences[page]
+        else:
+            result += pages[pages.shape[0] // 2]
+            
+    print(result)
+    print("Second")
+    result = 0
+    for update in bad_updates:
+        # bubble sort
+        for i in range(update.shape[0] - 1, update.shape[0] // 2 - 1, -1):
+            for j in range(0, i):
+                if (update[j], update[j+1]) in rule_set:
+                    #swap
+                    update[j], update[j+1] = update[j+1], update[j]
+        # print(update)
+        result += update[update.shape[0] // 2]
+    
+    
+    print (result)
+import timeit
+print(timeit.timeit(code, number=1))
